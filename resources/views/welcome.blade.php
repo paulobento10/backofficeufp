8fee2417cc4f015f8ad5b3f1b394a76f8742f2e7
paulobento10 <paulobento1997@gmail.com>
2019-05-01 15:16:30 +0100

    Initial ServiceWorker is working

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,6 +62,19 @@
                 margin-bottom: 30px;
             }
         </style>
+        <script>
+            if ('serviceWorker' in navigator ) {
+              window.addEventListener('load', function() {
+                  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+                      // Registration was successful
+                      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                  }, function(err) {
+                      // registration failed :(
+                      console.log('ServiceWorker registration failed: ', err);
+                  });
+              });
+          }
+        </script>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
